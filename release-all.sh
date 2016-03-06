8be1f7bd83d8c3f3987a9f347455173050180682
Robert Kohanyi <robert.kohanyi@metapack.com>
2016-03-06 12:28:47 +0000

    Update release-all.sh

--- release-all.sh
+++ release-all.sh
@@ -1,6 +1,12 @@
+mkdir -p release
+cd build
 for cross_target in {{linux,win32}-{ia32,x64},darwin-x64}
 do
-  mkdir -p release
-  cd build
-  zip ../release/$cross_target $cross_target/tagit{,.exe}
+  echo $cross_target | grep -Pq '^win32-(ia32|x64)$'
+  if [ $? -eq 0 ]
+  then
+    zip --must-match ../release/$cross_target $cross_target/tagit.exe
+  else
+    zip --must-match ../release/$cross_target $cross_target/tagit
+  fi
 done
